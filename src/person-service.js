ac3cc3fde1161d4055bb82a21de77b502ad46a6b
ydvorzhetskiy <dvorzhetskij@mail.ru>
2019-03-14 07:45:07 +0600

    Person-service

---
+++ src/person-service.js
@@ -0,0 +1,22 @@
+class PersonService {
+
+    getByName(name) {
+        // TODO: return by name
+        return null;
+    }
+
+    getAll() {
+        // TODO: return all
+        return null;
+    }
+
+    deleteByName(name) {
+        // TODO: delete by namr
+    }
+
+    save(person) {
+        // TODO: find if exists
+        // TODO: update if exist
+        // TODO: insert id exist
+    }
+}
